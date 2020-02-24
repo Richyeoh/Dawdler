de29f78bded0ac4f8c0780c5ee6bfb30f9a1f426
泽被苍生 <56948566+Richyeohx@users.noreply.github.com>
2020-02-24 21:07:07 +0800

    Add class "Brands.java" to get phone brands

---
+++ dawdler/src/main/java/com/richyeoh/android/dawdler/Brands.java
@@ -0,0 +1,34 @@
+package com.richyeoh.android.dawdler;
+
+import android.os.Build;
+
+public class Brands {
+    public static boolean isXiaomi() {
+        return Build.BRAND != null && Build.BRAND.toLowerCase().equals("xiaomi");
+    }
+
+    public static boolean isOppo() {
+        return Build.BRAND != null && Build.BRAND.toLowerCase().equals("oppo");
+    }
+
+    public static boolean isVivo() {
+        return Build.BRAND != null && Build.BRAND.toLowerCase().equals("vivo");
+    }
+
+    public static boolean isMeizu() {
+        return Build.BRAND != null && Build.BRAND.toLowerCase().equals("meizu");
+    }
+
+    public static boolean isHuawei() {
+        return Build.BRAND != null && Build.BRAND.toLowerCase().equals("huawei")
+                || Build.BRAND != null && Build.BRAND.toLowerCase().equals("honor");
+    }
+
+    public static boolean isSamsung() {
+        return Build.BRAND != null && Build.BRAND.toLowerCase().equals("samsung");
+    }
+
+    public static boolean isSmartisan() {
+        return Build.BRAND != null && Build.BRAND.toLowerCase().equals("smartisan");
+    }
+}
