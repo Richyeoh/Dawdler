00895ad06a5bac8093e4471bfb93ea0abe6da2cc
泽被苍生 <56948566+Richyeohx@users.noreply.github.com>
2020-02-26 00:22:45 +0800

    Test if ActivityOps can start Activity

--- app/src/main/java/com/richyeoh/android/app/dawdler/MainActivity.kt
+++ app/src/main/java/com/richyeoh/android/app/dawdler/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.richyeoh.android.app.dawdler
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.richyeoh.android.dawdler.ActivityOps
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startSecond(view: View) {
+        ActivityOps.startActivity(SecondActivity::class.java)
     }
 }
