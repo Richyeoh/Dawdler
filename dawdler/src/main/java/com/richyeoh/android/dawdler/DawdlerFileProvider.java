7f8b8b9086b4cd18cd2d3e8844f377386c84e74c
泽被苍生 <56948566+Richyeohx@users.noreply.github.com>
2020-02-28 14:00:58 +0800

    Modify "DawdlerFileProvider.java" Visible inside the module

--- dawdler/src/main/java/com/richyeoh/android/dawdler/DawdlerFileProvider.java
+++ dawdler/src/main/java/com/richyeoh/android/dawdler/DawdlerFileProvider.java
@@ -2,7 +2,7 @@ package com.richyeoh.android.dawdler;
 
 import androidx.core.content.FileProvider;
 
-public final class DawdlerFileProvider extends FileProvider {
+class DawdlerFileProvider extends FileProvider {
     @Override
     public boolean onCreate() {
         Utils.init();
