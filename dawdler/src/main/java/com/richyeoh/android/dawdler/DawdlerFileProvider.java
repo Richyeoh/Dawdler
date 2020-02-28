c143cedaa7f6dfe39c4759280d12fbe2e694f15b
泽被苍生 <56948566+Richyeohx@users.noreply.github.com>
2020-02-28 18:12:16 +0800

    Fixed unreachable exceptions caused by modifying class modifiers

--- dawdler/src/main/java/com/richyeoh/android/dawdler/DawdlerFileProvider.java
+++ dawdler/src/main/java/com/richyeoh/android/dawdler/DawdlerFileProvider.java
@@ -2,7 +2,7 @@ package com.richyeoh.android.dawdler;
 
 import androidx.core.content.FileProvider;
 
-final class DawdlerFileProvider extends FileProvider {
+public final class DawdlerFileProvider extends FileProvider {
     @Override
     public boolean onCreate() {
         Utils.init();
