ce5c9fbcd620f22a0814124dc48fe2c6b4db6fd6
泽被苍生 <56948566+Richyeoh@users.noreply.github.com>
2020-03-21 16:21:26 +0800

    Rename DawdlerFileProvider to DawdlerInitializer

--- dawdler/src/main/java/com/richyeoh/android/dawdler/DawdlerInitializer.java
+++ dawdler/src/main/java/com/richyeoh/android/dawdler/DawdlerInitializer.java
@@ -2,7 +2,7 @@ package com.richyeoh.android.dawdler;
 
 import androidx.core.content.FileProvider;
 
-public final class DawdlerFileProvider extends FileProvider {
+public final class DawdlerInitializer extends FileProvider {
     @Override
     public boolean onCreate() {
         Utils.init();
