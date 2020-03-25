2e90d98b627ff5e908bd8fcfa11c44ac58f183ce
泽被苍生 <56948566+Richyeoh@users.noreply.github.com>
2020-03-25 16:01:51 +0800

    Newly added AutoClearedValue class help preventing memory leaks

---
+++ dawdler-ktx/src/main/java/com/richyeoh/android/dawdler/AutoClearedValue.kt
@@ -0,0 +1,39 @@
+package com.richyeoh.android.dawdler
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.observe
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class AutoClearedValue<T : Any>(private val fragment: Fragment) : ReadWriteProperty<Fragment, T> {
+    private var _value: T? = null
+
+    init {
+        fragment.lifecycle.addObserver(object : DefaultLifecycleObserver {
+            override fun onCreate(owner: LifecycleOwner) {
+                super.onCreate(owner)
+                fragment.viewLifecycleOwnerLiveData.observe(fragment) { viewLifecycleOwner ->
+                    viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
+                        override fun onDestroy(owner: LifecycleOwner) {
+                            super.onDestroy(owner)
+                            _value = null
+                        }
+                    })
+                }
+            }
+        })
+    }
+
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+        return _value
+            ?: throw IllegalArgumentException("Value cannot be get.")
+    }
+
+    override fun setValue(thisRef: Fragment, property: KProperty<*>, value: T) {
+        _value = value
+    }
+}
+
+fun <T : Any> Fragment.autoCleared() = AutoClearedValue<T>(this)
